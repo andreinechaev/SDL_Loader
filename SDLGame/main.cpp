b0651af7e5e76b26771d74b4ced65bc9a7f1a71c
LincolnFaradey <iphoenix179@gmail.com>
2014-10-23 10:40:00 -0400

    Initial Commit

---
+++ SDLGame/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SDLGame
+//
+//  Created by Andrei Nechaev on 10/23/14.
+//  Copyright (c) 2014 Andrei Nechaev. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
